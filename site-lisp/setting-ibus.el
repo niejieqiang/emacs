8175aebbe453f1d9fa994cfa0ae35c1f25d92803
niejieqiang <niejieqiang@qq.com>
2011-10-27 12:24:13 +0800

    del ## file

--- site-lisp/setting-ibus.el
+++ site-lisp/setting-ibus.el
@@ -1,3 +1,4 @@
+(if (eq system-type 'windows-nt)
 (require 'ibus);; Turn on ibus-mode automatically after loading .emacs
-(add-hook 'after-init-hook 'ibus-mode-on)
+(add-hook 'after-init-hook 'ibus-mode-on))
 
